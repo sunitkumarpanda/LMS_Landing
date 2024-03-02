03e19cabcce344b2045f4d5880dd331c1f1e2406
Raj kishore Behera <115730942+Bablu08@users.noreply.github.com>
2024-03-02 12:39:04 +0530

    Update script.js

--- script.js
+++ script.js
@@ -4,3 +4,25 @@ const scroll = new LocomotiveScroll({
     el: document.querySelector('.main'),
     smooth: true
 });
+
+// Page animation.
+
+function firstpageAnim(){
+    var tl = gsap.timeline();
+
+    tl.from(".nav", {
+        y: '-10',
+        opacity: 0,
+        duration: 1.5,
+        ease: Expo.easeInOut
+    })
+    .to("span",{
+        y: 0,
+        ease: Expo.easeInOut,
+        duration: 2,
+        delay: -2,
+        stagger: .2
+     })
+}
+
+firstpageAnim();
